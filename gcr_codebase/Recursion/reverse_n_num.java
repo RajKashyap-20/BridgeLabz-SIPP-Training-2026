38eea5253c72b0d77555a1b80f8d91ca2bc4642c
RajKashyap-20 <rajkashyap.ki05@gmail.com>
2026-06-16 13:03:00 +0530

    5th

---
+++ gcr_codebase/Recursion/reverse_n_num.java
@@ -0,0 +1,18 @@
+package gcr_codebase.Recursion;
+
+import java.util.Scanner;
+
+public class reverse_n_num {
+    public static void printNumbers(int n) {
+        if (n < 1) {
+            return;
+        }
+        System.out.print(n + " ");
+        printNumbers(n - 1);
+    }
+    public static void main(String [] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        printNumbers(n);
+    }
+}
